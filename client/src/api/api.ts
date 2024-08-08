cc3dff536f9976f772bf8542f81bf446b8a64476
Danil <dev.dg08@google.com>
2024-08-08 12:23:38 +0300

    -welcome text tg

---
+++ client/src/api/api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+interface GameDataType {
+	energy: number;
+	coins: number;
+}
+
+const API_BASE_URL = 'http://127.0.0.1:8002/test';
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export const getGameData = async () => {
+  try {
+    const response = await api.get('/user_entry_check/0');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching game data:', error);
+    throw error;
+  }
+};
+
+export const createGameData = async (data: GameDataType) => {
+  try {
+    const response = await api.post(`/user_exit/${0}`, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error updating game data:', error);
+    throw error;
+  }
+};
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,8 +3,28 @@ import { Counter } from "./shared/Counter";
 import { Container } from "./shared/Container";
 import { Clicker } from "./shared/Clicker";
 import { EnergyBar } from "./shared/EnergyBar";
+import { useEffect } from "react";
+import { Telegraf, Markup } from "telegraf";
+
+const token = "7480673844:AAGi0IdX6ULIyLjJsXjrsbMxionAAd-uN_0";
+const webAppUrl = "https://clicker-fruit.vercel.app/";
+
+const bot = new Telegraf(token);
 
 function App() {
+  useEffect(() => {
+    bot.command("start", (ctx) => {
+      ctx.reply(
+        "Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение",
+        Markup.keyboard([
+          Markup.button.webApp("Отправить сообщение", `${webAppUrl}/feedback`),
+        ])
+      );
+    });
+
+    bot.launch();
+  }, []);
+
   return (
     <Container>
       <Counter />
